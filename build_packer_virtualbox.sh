e53a4d9bd63a69cbcc0a507ae0a621adfc48eab8
apolloclark <apolloclark@gmail.com>
2018-02-01 12:16:28 -0500

    removed unused packer options

--- build_packer_virtualbox.sh
+++ build_packer_virtualbox.sh
@@ -4,8 +4,11 @@ start=`date +%s`
 # remove old OVF file
 rm -rf ./output-*-virtualbox-ovf
 
-packer build -only=virtualbox-ovf \
-	packer_virtualbox.json
+packer validate packer_virtualbox.json
+
+packer inspect packer_virtualbox.json
+
+packer build packer_virtualbox.json
 
 end=`date +%s`
 secs=$((end-start))
