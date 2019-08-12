b255236cd3b432210881d8b52f3a7e9ca8242708
apolloclark <apolloclark@gmail.com>
2019-08-12 16:28:18 -0400

    added Amazon Linux 2

---
+++ base/spec/Dockerfile_amzlinux2.rb
@@ -0,0 +1,92 @@
+# spec/Dockerfile_spec.rb
+
+require "serverspec"
+require "docker"
+
+Docker.validate_version!
+
+describe "Dockerfile" do
+  before(:all) do
+    docker_username = "apolloclark"
+    package_name    = "openjdk"
+    package_version = "11"
+    image_name      = "amzlinux2"
+
+    # check for package version major usage
+    if package_version.match(/(\d+).x/)
+        package_version = package_version.match(/(\d+).x/)[1]
+    end
+
+    image = Docker::Image.get(
+      "#{docker_username}/#{package_name}:#{package_version}-#{image_name}"
+    )
+
+    # https://github.com/mizzy/specinfra
+    # https://docs.docker.com/engine/api/v1.24/#31-containers
+    # https://github.com/swipely/docker-api
+    # https://serverspec.org/resource_types.html
+    set :os, family: :redhat
+    set :backend, :docker
+    set :docker_image, image.id
+  end
+
+  def os_version
+    command("cat /etc/*-release").stdout
+  end
+
+  def sys_user
+    command("whoami").stdout.strip
+  end
+
+
+
+  it "installs the right version of CentOS" do
+    expect(os_version).to include("Amazon Linux")
+    expect(os_version).to include("Linux 2")
+  end
+
+  it "runs as root" do
+    expect(sys_user).to include("root")
+  end
+
+
+  # packages
+  describe package("java-11-amazon-corretto-devel") do
+    it { should be_installed }
+  end
+
+  describe command("java --version") do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match(/11(.*)/) }
+  end
+
+  describe command("javac --version") do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match(/11(.*)/) }
+  end
+
+  describe command("jshell --version") do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match(/11(.*)/) }
+  end
+
+  describe command('bash -l -c "echo $JAVA_HOME"') do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match("/usr/local/openjdk-11") }
+  end
+
+  describe command('bash -c "ls -lah $JAVA_HOME"') do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match("/usr/local/openjdk-11 ->") }
+  end
+
+  describe command('bash -c "ls $JAVA_HOME"') do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match("bin") }
+    its(:stdout) { should match("conf") }
+    its(:stdout) { should match("include") }
+    its(:stdout) { should match("legal") }
+    its(:stdout) { should match("lib") }
+    its(:stdout) { should match("release") }
+  end
+end
