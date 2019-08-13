35701c8e580648b416789683ee32824ea12af49e
apolloclark <apolloclark@gmail.com>
2019-08-13 08:44:29 -0400

    refactor

--- base/scripts/install_basics.sh
+++ base/scripts/install_basics.sh
@@ -10,34 +10,27 @@ if [ -x "$(command -v apt-get)" ]; then
     apt-get upgrade -yq
     apt-get install -yq aptitude software-properties-common python-minimal \
       nano curl wget git gnupg2 apt-transport-https
+    python --version
 
     mkdir -p /usr/lib/jvm/java-11-openjdk-amd64
     ln -s /usr/lib/jvm/java-11-openjdk-amd64/ $JAVA_HOME
-
-    exit 0;
-fi
-
-if [ -x "$(command -v dnf)" ]; then
+elif [ -x "$(command -v dnf)" ]; then
     dnf makecache
-    dnf --assumeyes install which nano curl wget git gnupg2 initscripts hostname python3
+    dnf --assumeyes install which nano curl wget git gnupg2 initscripts \
+        hostname python3
     dnf clean all
     alternatives --set python /usr/bin/python3
-    /usr/bin/python --version
+    python --version
 
     mkdir -p /usr/lib/jvm/java-11-openjdk-11.0.4.11-0.el8_0.x86_64
     ln -s /usr/lib/jvm/java-11-openjdk-11.0.4.11-0.el8_0.x86_64/ $JAVA_HOME
-
-    exit 0;
-fi
-
-if [ -x "$(command -v yum)" ]; then
+elif [ -x "$(command -v yum)" ]; then
     yum makecache fast
     yum update -y
     yum install -y which nano curl wget git gnupg2 initscripts hostname
     yum clean all
+    python --version
 
     mkdir -p /usr/lib/jvm/java-11-openjdk-11.0.4.11-0.el7_6.x86_64
     ln -s /usr/lib/jvm/java-11-openjdk-11.0.4.11-0.el7_6.x86_64/ $JAVA_HOME
-
-    exit 0;
 fi
